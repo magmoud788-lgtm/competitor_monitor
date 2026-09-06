5d76e11d568a8c263f723ca0c399ab788906d5ef
magmoud <magmoud788@gmail.com>
2026-09-06 21:45:31 +0400

    fixed queries createuser function

--- db/queries.js
+++ db/queries.js
@@ -3,7 +3,8 @@ const pool = require('./pool');
 // --- users ---
 const createUser = (name, email, passwordHash) =>
   pool.query(
-    `INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3)
+    `INSERT INTO public.users (name, email, password_hash)
+     VALUES ($1, $2, $3)
      RETURNING id, name, email, created_at`,
     [name, email, passwordHash]
   );
